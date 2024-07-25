b1887b0468367fb405809aa2edbae51964e6578f
lukethorvilson <luke.thorvilson@wsu.edu>
2024-07-25 15:40:59 -0700

    Setup react router browser router.

--- Portfolio of Luke Thorvilson/src/App.jsx
+++ Portfolio of Luke Thorvilson/src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ModeProvider } from "./contexts/ModeProvider";
 import Logo from "./components/Logo";
 
 function App() {
-  return <><Logo /></>;
+  return (
+    <ModeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Logo />} />
+          {/* set up routes to pages here and layout */}
+          <Route path="*" element={<h1>Page Not Found</h1>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ModeProvider>
+  );
 }
 
 export default App;
