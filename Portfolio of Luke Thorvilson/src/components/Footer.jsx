cdd6345bd078f7b074887ae6234a22d58eec5e8b
lukethorvilson <luke.thorvilson@wsu.edu>
2024-08-21 13:27:40 -0700

    Fixed footer so links adjust to screen size

--- Portfolio of Luke Thorvilson/src/components/Footer.jsx
+++ Portfolio of Luke Thorvilson/src/components/Footer.jsx
@@ -10,24 +10,24 @@ import { FaGithub } from "react-icons/fa";
 function Footer() {
   return (
     <footer className="bottom-0 h-48 w-full content-center bg-zinc-700">
-      <p className="mb-7 text-center text-white">
+      <p className="mb-7 text-center text-sm sm:text-base text-white">
         © Luke R. Thorvilson 2024. All rights reserved.
       </p>
       <div className="flex justify-center gap-5">
         <Link to="https://www.linkedin.com/in/luke-thorvilson-2ba470252/">
-          <FaLinkedin className="h-10 w-10 text-white" />
+          <FaLinkedin className="h-6 w-6 sm:h-10 sm:w-10 text-white" />
         </Link>
         <Link to="https://mail.google.com/mail/?view=cm&fs=1&to=luke.thorvilson.work@gmail.com&su=SUBJECT&body=BODY">
-          <IoIosMail className="h-10 w-10 text-white" />
+          <IoIosMail className="h-6 w-6 sm:h-10 sm:w-10 text-white" />
         </Link>
         <Link to="https://x.com/MarinersNerd">
-          <FaXTwitter className="h-10 w-10 text-white" />
+          <FaXTwitter className=" h-6 w-6 sm:h-10 sm:w-10 text-white" />
         </Link>
         <Link to="https://wsu.joinhandshake.com/profiles/28954228">
-          <SiHandshake className="h-10 w-10 text-white" />
+          <SiHandshake className="h-6 w-6 sm:h-10 sm:w-10 text-white" />
         </Link>
         <Link to="https://github.com/lukethorvilson">
-          <FaGithub className="h-10 w-10 text-white" />
+          <FaGithub className="h-6 w-6 sm:h-10 sm:w-10 text-white" />
         </Link>
       </div>
     </footer>
